42e8714bbcd9893aea0f7965ac0c66f49f2b37ca
Jerome Swannack <jeromeswannack@gmail.com>
2023-02-20 22:22:52 +1300

    Added training data generation for facebook messages

--- facebook/generate_training.py
+++ facebook/generate_training.py
@@ -4,18 +4,27 @@ from pydantic import BaseModel
 _FB_DATA_ROOT = "~/source/bigdata/data/facebook/"
 _BOT_NAME = "Jerome Swannack"
 _END_TOKEN = ""
+_BLOCKED_USER = "Facebook User" 
+_BLOCKED_USER_IN_MESSAGE = "Other user"
 
-class Messages(BaseModel):
+class Message(BaseModel):
     sender_name: str
     timestamp_ms: int
-    content: str
+    content: str = None
     type: str
     is_unsent: bool
 
 
+class User(BaseModel):
+    name: str
+
 class Conversation(BaseModel):
-    participants: list[str]
-    messages: list[dict]
+    participants: list[User]
+    messages: list[Message]
+
+    def contains_any_blocked_user(self):
+        # check if any participant is "Facebook User"
+        return any(p.name == _BLOCKED_USER for p in self.participants)
 
 
 def _get_all_message_files(root: Path):
@@ -43,19 +52,41 @@ def _convert_conversation_to_training_text(conversation: Conversation):
         if message.type != "Generic":
             continue
 
+        # skip messages with no content
+        if not message.content:
+            continue
+
+        # skip messages from blocked users
+        if message.sender_name == _BLOCKED_USER_IN_MESSAGE:
+            continue
+
         s += f"{message.sender_name}: {message.content}\n"
         if message.sender_name == _BOT_NAME:
             s += f"{_END_TOKEN}\n"
 
+    return s
+
 
 def main():
+    # get the first 10 conversations and print the training output
+
+    i = 0
     for conversation in _get_all_conversations(_FB_DATA_ROOT):
+        if conversation.contains_any_blocked_user():
+            continue
+
         print(_convert_conversation_to_training_text(conversation))
-        break
+        i += 1
+        if i == 10:
+            break
+
 
 
 if __name__ == "__main__":
     import logging
     logging.basicConfig(level=logging.INFO)
-    main()
+
+    import ipdb
+    with ipdb.launch_ipdb_on_exception():
+        main()
 
